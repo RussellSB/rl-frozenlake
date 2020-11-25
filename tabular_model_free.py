396048a20832984335b961f6fcbb7cc7e1c205b9
Gunar A. Kroeger <gunar.kroeger@gmail.com>
2020-11-25 16:07:11 +0000

    q learning first implementation

--- tabular_model_free.py
+++ tabular_model_free.py
@@ -67,15 +67,41 @@ def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
 def q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
     random_state = np.random.RandomState(seed)
 
-    eta = np.linspace(eta, 0, max_episodes)
+    eta = np.linspace(eta, 0, max_episodes) #learning_rate?
     epsilon = np.linspace(epsilon, 0, max_episodes)
 
     q = np.zeros((env.n_states, env.n_actions))
 
+    sum_rewards = np.zeros(env.n_actions)
+    times_action_observed = np.zeros(env.n_actions)
+    timestep = 0
     for i in range(max_episodes):
         s = env.reset()
-        # TODO:
+        while(s != env.absorbing_state):
+            #Select action a_prime for state s_prime according to an e-greedy policy based on Q
+            if(timestep < env.n_actions):
+                a = timestep #select each action once
+                #print(f'sum_rewards = {sum_rewards}')
+            else:
+                mean_rewards = sum_rewards / times_action_observed
+                best_action = randomBestAction(mean_rewards)
+                if(np.random.random(1) < epsilon[i]):
+                    a = best_action
+                else:
+                    a = random.randrange(env.n_actions)
+                #print(f'mean_rewards = {mean_rewards}')
+                #print(f'best_action = {best_action}')
+            timestep += 1
 
+            s_prime, r, done = env.step(a)
+            sum_rewards[a] += r
+            times_action_observed[a] += 1
+            mean_rewards = sum_rewards / times_action_observed
+            a_prime = randomBestAction(mean_rewards)
+            q[s,a] = q[s,a] + eta[i] * (r + gamma * q[s_prime, a_prime] - q[s,a])
+            s = s_prime
+            a = a_prime
+        ###### end of our code ##########
     policy = q.argmax(axis=1)
     value = q.max(axis=1)
 
