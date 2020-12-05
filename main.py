e56f5e4499129437b6e0df3ac3efae134cd98a2d
Gunar A. Kroeger <gunar.kroeger@gmail.com>
2020-12-05 18:37:59 +0000

    added big lake

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ def main():
     seed = 0
 
     # Small lake
-    lake = [['&', '.', '.', '.'],
-            ['.', '#', '.', '#'],
-            ['.', '.', '.', '#'],
-            ['#', '.', '.', '$']]
+    small_lake =    [['&', '.', '.', '.'],
+                    ['.', '#', '.', '#'],
+                    ['.', '.', '.', '#'],
+                    ['#', '.', '.', '$']]
 
-    env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
+    big_lake =      [['&', '.', '.', '.', '.', '.', '.', '.'],
+                    ['.', '.', '.', '.', '.', '.', '.', '.'],
+                    ['.', '.', '.', '#', '.', '.', '.', '.'],
+                    ['.', '.', '.', '.', '.', '#', '.', '.'],
+                    ['.', '.', '.', '#', '.', '.', '.', '.'],
+                    ['.', '#', '#', '.', '.', '.', '#', '.'],
+                    ['.', '#', '.', '.', '#', '.', '#', '.'],
+                    ['.', '.', '.', '#', '.', '.', '.', '$']]
+
+    env = FrozenLake(small_lake, slip=0.1, max_steps=16, seed=seed)
     # env.play()
 
     print('# Model-based algorithms')
@@ -22,29 +31,29 @@ def main():
     theta = 0.001
     max_iterations = 10000
 
-    # print('')
-    #
-    # print('## Policy iteration')
-    # policy, value = policy_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
-    #
-    # print('')
-    #
-    # print('## Value iteration')
-    # policy, value = value_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
-    #
+    print('')
+
+    print('## Policy iteration')
+    policy, value = policy_iteration(env, gamma, theta, max_iterations)
+    env.render(policy, value)
+
+    print('')
+
+    print('## Value iteration')
+    policy, value = value_iteration(env, gamma, theta, max_iterations)
+    env.render(policy, value)
+
     max_episodes = 2000
     eta = 0.5
     epsilon = 0.5
 
-    # print('# Model-free algorithms')
-    # print('## sarsa')
-    # policy, value = sarsa(env, max_episodes, eta, gamma, epsilon)
-    # env.render(policy, value)
-    # print('## q_learning')
-    # policy, value = q_learning(env, max_episodes, eta, gamma, epsilon)
-    # env.render(policy, value)
+    print('# Model-free algorithms')
+    print('## sarsa')
+    policy, value = sarsa(env, max_episodes, eta, gamma, epsilon)
+    env.render(policy, value)
+    print('## q_learning')
+    policy, value = q_learning(env, max_episodes, eta, gamma, epsilon)
+    env.render(policy, value)
 
     print('# Model-free algorithms')
     linear_env = LinearWrapper(env)
