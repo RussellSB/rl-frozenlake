ea1d72a9cfd9026d17a0dafa5fc4ba597e6ca08c
Gunar A. Kroeger <gunar.kroeger@gmail.com>
2020-12-07 21:25:11 +0000

    added finding number of episodes for optimal policy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from frozen_lake import FrozenLake
 from policy_iteration import policy_iteration
 from value_iteration import value_iteration
 from tabular_model_free import sarsa, q_learning
+import numpy as np
 from non_tabular_model_free import linear_q_learning, linear_sarsa, LinearWrapper
 
 
@@ -36,20 +37,19 @@ def main():
 
     print('')
 
-    # print('## Policy iteration')
-    # policy, value = policy_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
-    #
-    # print('')
-    #
-    # print('## Value iteration')
-    # policy, value = value_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
+    print('## Policy iteration')
+    policy, value = policy_iteration(env, gamma, theta, max_iterations)
+    env.render(policy, value)
+
+    print('')
+
+    print('## Value iteration')
+    optimal_policy, value = value_iteration(env, gamma, theta, max_iterations)
+    env.render(optimal_policy, value)
 
     max_episodes = 2000
     eta = 0.5
     epsilon = 0.5
-
     print('# Model-free algorithms')
     print('## sarsa')
     policy, value = sarsa(env, max_episodes, eta, gamma, epsilon, seed=seed)
@@ -71,5 +71,20 @@ def main():
     policy, value = linear_env.decode_policy(parameters)
     linear_env.render(policy, value)
 
+    print('# finding number of episodes for optimal policy')
+    for episodes in np.arange(500,5000,100):
+        print(f'sarsa episodes = {episodes}')
+        policy, value = sarsa(env, episodes, eta, gamma, epsilon, seed=seed)
+        policy[15] = 0  # set the policy for the goal state as 0 to compare with optimal policy
+        if np.array_equal(policy, optimal_policy):
+            break
+    env.render(policy, value)
 
+    for episodes in np.arange(500,5000,100):
+        print(f'q_learning episodes = {episodes}')
+        policy, value = q_learning(env, episodes, eta, gamma, epsilon, seed=seed)
+        policy[15] = 0  # set the policy for the goal state as 0 to compare with optimal policy
+        if np.array_equal(policy, optimal_policy):
+            break
+    env.render(policy, value)
 main()
